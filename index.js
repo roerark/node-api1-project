d7939c319f47fd1c09c6d9e74cb03bec48306661
joe-park <parkjs3@yahoo.com>
2021-07-06 22:29:31 -0700

    mvp IG

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 5000;
 
 // START YOUR SERVER HERE
+console.log("hey you!");
+
+server.listen(port, () => {
+  console.log("listening on", port);
+});
